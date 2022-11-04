6d810d42590eee90cbf33585be24fb1c2962c439
VictorCstr <victorcastro.dev@hotmail.com>
2022-11-03 22:10:36 -0300

    test: test jwt user login, and hash on password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,16 +1,28 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 const userController = {
   createUser: async (req, res) => {
     try {
       let fields = req.body;
+
       if (fields.name == "" || fields.email == "" || fields.password == "") {
         return res.status(400).json({ error: "Fields cannot be empty" });
       }
+
+      let userAlreadyExist = await User.findOne({ email: req.body.email });
+
+      if (userAlreadyExist)
+        return res.status(400).json({ error: "Invalid email" });
+
+      const pass = req.body.password;
+      const salt = await bcrypt.genSalt(10);
+      let hash = await bcrypt.hash(pass, salt);
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hash,
       });
       await newUser.save();
       return res.status(200).json({ email: req.body.email });
